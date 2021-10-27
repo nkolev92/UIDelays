2a426840acdcd7f184217f23e7f6ac59d4ddfa66
Nikolche Kolev <nikolev@microsoft.com>
2021-10-26 18:54:57 -0700

    Cleanup

---
+++ LegacyPackagesConfigProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyPackagesConfigProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
